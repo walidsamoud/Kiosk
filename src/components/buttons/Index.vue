16564b6b36a5c11124b1787e719992b86011d29e
Walid Sammoud <walid@liberrex.com>
2021-04-22 14:18:23 +0100

    Btns added

---
+++ src/components/buttons/Index.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="col">
+        <button :class="'btn theme-'+theme+' size-'+size"> {{ name }} </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Dark button',
+        props: {
+            name: String,
+            size: String,
+            theme: String
+        }
+    }
+</script>
+
+<style scoped>
+    .btn{
+        width: 100%;
+        border-radius: 0px;
+        font-size: 20px;
+        background-color: #193060;
+        color: #fff;
+        cursor: pointer;
+        transition: .3s;
+    }
+    .btn:hover{
+        opacity: .9;
+        color: #fff;
+    }
+    .theme-dark{
+        background-color: #193060;
+        color: #fff;
+    }
+    .theme-medium{
+        background-color: #264076;
+        color: #fff;
+    }
+    .theme-light{
+        background-color: #294786;
+        color: #fff;
+    }
+    .size-large{
+        font-size: 30px;
+        height: 100px;
+    }
+    .size-medium{
+        font-size: 25px;
+        height: 75px;
+    }
+    .size-small{
+        font-size: 20px;
+        height: 50px;
+    }
+</style>
--- src/views/test_components/Index.vue
+++ src/views/test_components/Index.vue
@@ -1,26 +1,71 @@
 <template>
     <div class="language_buttons">
-        <div class="r">
-            <langAr></langAr>
+        <div class="row background-ad">
+            <div class="col">
+                <!-- <img class="img-fluid" src="../../assets/images/test_ad.png" alt="..."> -->
+                <img class="img-fluid" src="https://i0.wp.com/concours-tunisie.tn/wp-content/uploads/2019/04/Concours-Tunisie-Telecom.png?fit=1280%2C720&ssl=1" alt="">
+            </div>
+        </div>
+        <div class="row bottom-btns">
+            <div class="col">
+                <Button name="English" size="large" theme="dark"></Button>
+            </div>
+            <div class="col">
+                <Button name="Francais" size="large" theme="medium"></Button>
+            </div>
+            <div class="col">
+                <Button name="العربية" size="large" theme="light"></Button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
-import LangAr from './../../components/buttons/Lang_ar.vue';
+import { defineComponent } from '@vue/composition-api';
+import Button from '../../components/buttons/Index.vue';
+
+import $ from 'jquery'
 
 export default defineComponent({
     name: "TestContainer",
     components: {
-        LangAr
+        Button,
     },
+    mounted: function(){
+        $('.img-fluid').css("height", screen.height-100);
+    }
 })
 </script>
 
 <style scoped>
+    *{
+         overflow: hidden;
+    }
+    html,body { height: 100%; width: 100%; margin: 0px; padding: 0px;}
     .language_buttons{
-        position: absolute;
+        margin-top: -60px;
+    }
+    .background-ad{
+        text-align: center;
+        width: 100%;
+        z-index: 1;
+    }
+    .background-ad img{
+        top: 0;
+        padding: 0;
+        background-size: contain;
+        width: 100%;
+    }
+    .bottom-btns{
+        position: fixed;
         bottom: 0px;
+        margin: 0;
+        width: 100%;
+        z-index: 9;
+    }
+    .col{
+        width: 100%;
+        padding-right:0;
+        padding-left:0;
     }
 </style>
