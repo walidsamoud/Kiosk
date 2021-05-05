25ca6bcaf71c00af5bd95033858ce09abbace52d
iheb <iheb@liberrex.com>
2021-05-05 01:19:38 +0000

    Updated router.js

--- src/_helpers/router.js
+++ src/_helpers/router.js
@@ -63,8 +63,6 @@ export const router = new Router({
                     },
                     ],
                 },
-                  // component: () => import('../views/kiosk/Home')
-                component: () => import('../views/kiosk/ServicesPage')
             },
             {
                 path: '/services',
